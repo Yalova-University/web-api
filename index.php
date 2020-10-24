49bd88585e75d0b72a0fc4f4cc53402724561b42
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2020-10-24 12:29:04 +0300

    index.php update

--- index.php
+++ index.php
@@ -1,47 +1,53 @@
 <?php
+$site = file_get_contents("http://yalova.edu.tr/");
+
+preg_match_all('@<div><a href="(.*?)">@si', $site, $sliderlink);
+$sliderlinkcikti = implode("^", $sliderlink[1]);
+$sliderlinkdizi = explode ("^",$sliderlinkcikti);
+
 
 
 $person = array(
 'sliders' =>array(
     'slider_1' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi0"
     ),
     'slider_2' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi1"
     ),
     'slider_3' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi2"
     ),
     'slider_4' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi3"
     ),
     'slider_5' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi4"
     ),
     'slider_6' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi5"
     ),
     'slider_7' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi6"
     ),
     'slider_8' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi7"
     ),
     'slider_9' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi8"
     ),
     'slider_0' =>array(
       "img_link"=>"abc@gfg.com",
-      "url"=>"abc@gfg.com"
+      "url"=>"$sliderlinkdizi9"
     ),
   )
 
