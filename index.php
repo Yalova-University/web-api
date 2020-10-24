c4e2ae524095337976ae613c9584fad7a9d34e9f
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2020-10-24 21:12:14 +0300

    index.php slider url update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-$site = file_get_contents("http://yalova.edu.tr/");
-
-preg_match_all('@<div><a href="(.*?)">@si', $site, $sliderlink);
+$site = file_get_contents("http://www.yalova.edu.tr/");
+$site2 = "http://www.yalova.edu.tr/";
+preg_match_all('@<img src="(.*?)">@si', $site, $sliderlink);
 $sliderlinkcikti = implode("^", $sliderlink[1]);
 $sliderlinkdizi = explode ("^",$sliderlinkcikti);
 
@@ -10,44 +10,44 @@ $sliderlinkdizi = explode ("^",$sliderlinkcikti);
 $person = array(
 'sliders' =>array(
     'slider_1' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi0"
+      "img_link"=>"$site2$sliderlinkdizi[6]",
+      "url"=>""
     ),
     'slider_2' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi1"
+      "img_link"=>"$site2$sliderlinkdizi[7]",
+      "url"=>""
     ),
     'slider_3' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi2"
+      "img_link"=>"$site2$sliderlinkdizi[8]",
+      "url"=>""
     ),
     'slider_4' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi3"
+      "img_link"=>"$site2$sliderlinkdizi[9]",
+      "url"=>""
     ),
     'slider_5' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi4"
+      "img_link"=>"$site2$sliderlinkdizi[10]",
+      "url"=>""
     ),
     'slider_6' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi5"
+      "img_link"=>"$site2$sliderlinkdizi[11]",
+      "url"=>""
     ),
     'slider_7' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi6"
+      "img_link"=>"$site2$sliderlinkdizi[12]",
+      "url"=>""
     ),
     'slider_8' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi7"
+      "img_link"=>"$site2$sliderlinkdizi[13]",
+      "url"=>""
     ),
     'slider_9' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi8"
+      "img_link"=>"$site2$sliderlinkdizi[14]",
+      "url"=>""
     ),
     'slider_0' =>array(
-      "img_link"=>"abc@gfg.com",
-      "url"=>"$sliderlinkdizi9"
+      "img_link"=>"$site2$sliderlinkdizi[15]",
+      "url"=>""
     ),
   )
 
