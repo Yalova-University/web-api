de65a52f59dde5a07160fa37994e012f8f297d7f
Umut Can Arda <47474804+FlySquare@users.noreply.github.com>
2020-10-24 12:13:57 +0300

    index.php slider
    basic slider template

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+
+$person = array(
+'sliders' =>array(
+    'slider_1' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_2' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_3' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_4' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_5' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_6' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_7' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_8' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_9' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+    'slider_0' =>array(
+      "img_link"=>"abc@gfg.com",
+      "url"=>"abc@gfg.com"
+    ),
+  )
+
+);
+echo $personJSON=json_encode($person);//returns JSON string
+
+
+
+
+
+ ?>
